ab921130f91b68a308fa8fbcb4214f7548829707
Danny Lambert <dlambert150293@gmail.com>
2021-09-12 15:13:52 +0100

    first commit to heroku

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Employees from "./Employees/employees"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// BOOTSTRAP
+// IFRAME
+// REACT?
+// ALL THE SHIT ABOVE
+// MONGODB SHIT 
+// THE SERVER STUFF? NODEMON?
+console.log("working")
+
+class App extends Component {
+  componentDidMount() {
+    document.title = "R2R Group Betting";
+  }
+    render(){
+      return(
+ 
+                  <Employees />                 
+
+       )
+     }
+
+    }
+
 
 export default App;
